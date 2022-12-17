b0c125143c2ad5e2da08c7a87897f6f018564c41
AbhayETH <abhaynarayan16122002@gmail.com>
2022-12-18 02:26:37 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import {fileURLToPath} from "url";
 import mongoose from "mongoose";
-import {register} from "./controllers/auth.js"
-import authRoutes from "./routes/auth.js"
+import {register} from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 // Configuration
 
@@ -45,6 +46,7 @@ app.post("/auth/register", upload.single("picture"), register)
 // ROUTES
 
 app.use("/auth",authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE setup
 
