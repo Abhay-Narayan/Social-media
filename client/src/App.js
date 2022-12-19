adec76cb51b1a835feeb9f18f32040b22d7a7917
AbhayETH <abhaynarayan16122002@gmail.com>
2022-12-19 18:09:57 +0530

    Frontens commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter, Navigate, Route, Routes} from react-router-dom;
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
+
+function App() {
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
